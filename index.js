28f4b8f4a075a253c72c1a3f1219f2d44cb9f346
rz-hub <76729848+rz-hub@users.noreply.github.com>
2021-02-14 12:30:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ const { ind } = require('./language')
 /********** MENU SETTING **********/
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
-            + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'FN:rzsbn\n' 
+            + 'ORG: Pengembang RZBOT;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6283846296813:+62 852-3186-5675\n' 
             + 'END:VCARD' 
 blocked = []   
 prefix = '#'
@@ -47,7 +47,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6283846296813@s.whatsapp.net","6285231865675@s.whatsapp.net"] 
 /************************************/
 
        
